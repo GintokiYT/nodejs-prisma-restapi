f587286e5333c96de5849beb81c3fcfce5be6ad1
Renato <devrenatonavarro@gmail.com>
2023-05-21 16:48:55 -0500

    first commit

--- prisma/migrations/20230521212225_first/migration.sql
+++ prisma/migrations/20230521212225_first/migration.sql
@@ -20,5 +20,13 @@ CREATE TABLE `Category` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Prueba` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Product` ADD CONSTRAINT `Product_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
